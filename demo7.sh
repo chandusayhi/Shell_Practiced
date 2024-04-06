e5d65626cc50883900776b7f84a4ed7140296041
Chandrakant Hatti <chandusayhi@users.noreply.github.com>
2024-04-06 21:06:47 +0530

    new update

--- demo7.sh
+++ demo7.sh
@@ -4,4 +4,5 @@ echo "clear command is used to clear the console"
 echo "history command ios used to display the list of commands worked in the console"
 echo "touch command is used to create the new files"
 echo "rm command is used to delete the files and directories"
+echo "New line"
 
